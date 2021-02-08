d56ddab18bc9168e2dc36fceb5bf26ff2d387935
august-alt <77973983+august-alt@users.noreply.github.com>
2021-02-08 13:47:14 +0400

    add xvfb and tests

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# setup xvfb
+Xvfb :1 -screen 0 1920×1080x16 &> xvfb.log  &
+
+export DISPLAY=:1.0
+
+# build tests
+cd /app
+
+cmake -B build -DFIND_MODULES=ON
+
+cmake --build build/ -j4
+
+./build/test_admc
+
+# make package
 cd /app/ && gear-rpm -ba
